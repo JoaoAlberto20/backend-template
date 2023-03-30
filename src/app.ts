319a199718a2cff2e0e426758146672737e6a765
Joao Alberto <joaoalbertosv15@gmail.com>
2023-03-30 11:51:58 -0300

    :wrench: chore: adicionando variaveis de ambiente e configurando o zod

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
 import fastify from 'fastify';
 
 import fastifyCookie from '@fastify/cookie';
+import fastifyJwt from '@fastify/jwt';
+
+import { env } from './config/env';
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+});
 
 app.register(fastifyCookie);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { app } from './app';
+import { env } from './config/env';
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
+    // eslint-disable-next-line no-console
     console.log('🚀 HTTP Server Running!');
   });
